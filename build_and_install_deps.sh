91a1dd11d59864b155c770c275177d389e89b179
Russell Standish <hpcoder@hpcoders.com.au>
2016-02-02 18:48:39 +1100

    openmpi-bin dependency needed

--- build_and_install_deps.sh
+++ build_and_install_deps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo apt-get install tcl8.5 tcl8.5-dev tk8.5 tk8.5-dev libreadline5 libboost-graph-dev libigraph0-dev libroot-math-unuran-dev libboost-graph-dev libreadline5 exuberant-ctags libdb-dev libdb++-dev mpd Xvfb openmpi -y
+sudo apt-get install tcl8.5 tcl8.5-dev tk8.5 tk8.5-dev libreadline5 libboost-graph-dev libigraph0-dev libroot-math-unuran-dev libboost-graph-dev libreadline5 exuberant-ctags libdb-dev libdb++-dev mpd Xvfb openmpi-bin -y
 
