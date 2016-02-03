bd6226ec22f7ab31918beb5b0d4717fc62dcff98
Russell Standish <hpcoder@hpcoders.com.au>
2016-02-03 12:41:28 +1100

    Add hpcoder packages for unuran as dependencies

--- build_and_install_deps.sh
+++ build_and_install_deps.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-sudo apt-get install tcl8.5 tcl8.5-dev tk8.5 tk8.5-dev libreadline5 libboost-graph-dev libigraph0-dev libroot-math-unuran-dev libboost-graph-dev libreadline5 exuberant-ctags libdb-dev libdb++-dev mpd Xvfb openmpi-bin -y
+wget http://download.opensuse.org/repositories/home:hpcoder1/xUbuntu_14.04/Release.key
+sudo apt-key add - < Release.key
+echo 'deb http://download.opensuse.org/repositories/home:/hpcoder1/xUbuntu_14.04/ /' >/tmp/hpcoders.list
+sudo mv /tmp/hpcoders.list /etc/apt/sources.list.d/
+sudo apt-get install tcl8.5 tcl8.5-dev tk8.5 tk8.5-dev libreadline5 libboost-graph-dev libigraph0-dev unuran-dev libboost-graph-dev libreadline5 exuberant-ctags libdb-dev libdb++-dev mpd Xvfb openmpi-bin -y
 
