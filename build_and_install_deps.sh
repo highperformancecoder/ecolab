93cd47cd9f2cfc61788d8a4ee136a3b1d2d56368
Russell Standish <hpcoder@hpcoders.com.au>
2016-02-03 12:44:15 +1100

    Should be libunuran-dev

--- build_and_install_deps.sh
+++ build_and_install_deps.sh
@@ -3,5 +3,5 @@ wget http://download.opensuse.org/repositories/home:hpcoder1/xUbuntu_14.04/Relea
 sudo apt-key add - < Release.key
 echo 'deb http://download.opensuse.org/repositories/home:/hpcoder1/xUbuntu_14.04/ /' >/tmp/hpcoders.list
 sudo mv /tmp/hpcoders.list /etc/apt/sources.list.d/
-sudo apt-get install tcl8.5 tcl8.5-dev tk8.5 tk8.5-dev libreadline5 libboost-graph-dev libigraph0-dev unuran-dev libboost-graph-dev libreadline5 exuberant-ctags libdb-dev libdb++-dev mpd Xvfb openmpi-bin -y
+sudo apt-get install tcl8.5 tcl8.5-dev tk8.5 tk8.5-dev libreadline5 libboost-graph-dev libigraph0-dev libunuran-dev libboost-graph-dev libreadline5 exuberant-ctags libdb-dev libdb++-dev mpd Xvfb openmpi-bin -y
 
