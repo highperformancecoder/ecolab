e893933ea04e2e630e36b171383dbf11496740b3
Russell Standish <hpcoder@hpcoders.com.au>
2016-02-03 13:31:40 +1100

    Add libprng-dev to ist of dependencies

--- build_and_install_deps.sh
+++ build_and_install_deps.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-wget http://download.opensuse.org/repositories/home:hpcoder1/xUbuntu_14.04/Release.key
-sudo apt-key add - < Release.key
-echo 'deb http://download.opensuse.org/repositories/home:/hpcoder1/xUbuntu_14.04/ /' >/tmp/hpcoders.list
-sudo mv /tmp/hpcoders.list /etc/apt/sources.list.d/
-sudo apt-get update -qq
-sudo apt-get install tcl8.5 tcl8.5-dev tk8.5 tk8.5-dev libreadline5 libboost-graph-dev libigraph0-dev libunuran-dev libboost-graph-dev libreadline5 exuberant-ctags libdb-dev libdb++-dev mpd Xvfb openmpi-bin -y
+sudo apt-get install tcl8.5 tcl8.5-dev tk8.5 tk8.5-dev libreadline5 libboost-graph-dev libigraph0-dev libprng-dev libunuran-dev libboost-graph-dev libreadline5 exuberant-ctags libdb-dev libdb++-dev mpd Xvfb openmpi-bin -y
 
