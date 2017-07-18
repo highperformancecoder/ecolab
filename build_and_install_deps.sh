0bb2185e554a49055c10fd1e7baeff759fcfa3f5
hpcoder@hpcoders.com.au <hpcoder@hpcoders.com.au>
2017-07-19 04:48:55 +1000

    Try xvfb instead of Xvfb for Travis

--- build_and_install_deps.sh
+++ build_and_install_deps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo apt-get install tcl8.5 tcl8.5-dev tk8.5 tk8.5-dev libreadline5 libboost-graph-dev libigraph0-dev libprng-dev libunuran-dev libboost-graph-dev libreadline5 exuberant-ctags libdb-dev libdb++-dev mpd Xvfb openmpi-bin -y
+sudo apt-get install tcl8.5 tcl8.5-dev tk8.5 tk8.5-dev libreadline5 libboost-graph-dev libigraph0-dev libprng-dev libunuran-dev libboost-graph-dev libreadline5 exuberant-ctags libdb-dev libdb++-dev mpd xvfb openmpi-bin -y
 
