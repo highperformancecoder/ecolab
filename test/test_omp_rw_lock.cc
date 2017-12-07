31a5a65b39fa0da390a1c4d9d8fcaff06f2370ab
Russell Standish <hpcoder@hpcoders.com.au>
2017-12-07 14:59:45 +1100

    fix subtle race condition in test_omp_rw_lock

--- test/test_omp_rw_lock.cc
+++ test/test_omp_rw_lock.cc
@@ -17,34 +17,36 @@ int main()
 #ifdef _OPENMP
 
   volatile bool running = true;
-  volatile unsigned read=0, write=0;
+  volatile unsigned rwflags=0;
   RWlock lock;
 #pragma omp parallel sections num_threads(3)
   {
     // Thread 0 check exclusion of read & write locks
 #pragma omp section
+    {
     while (running)
-      assert(!(read && write));
-
+      assert((rwflags&5) != 5);
+    }
+    
     // Thread 1 attempt read locks
 #pragma omp section
     {
       {
         read_lock r(lock);
 #pragma omp atomic
-        read |= 1;
+        rwflags |= 1;
         sleep(1); //wait for thread 2 to attempt write lock
 #pragma omp atomic
-        read &= ~1;
+        rwflags &= ~1;
       }
       sleep(1);
       {
         read_lock r(lock);
 #pragma omp atomic
-        read |= 1;
+        rwflags |= 1;
         sleep(1);
 #pragma omp atomic
-        read &= ~1;
+        rwflags &= ~1;
       }
       while (running);
     }
@@ -55,16 +57,17 @@ int main()
       {
         read_lock r(lock);
 #pragma omp atomic
-        read |= 2;
-        while (!(read&1)); //wait until Thread 1 sets readlock
-        assert(read==3); //check both read locks active
+        rwflags |= 2;
+        while (!(rwflags&1)); //wait until Thread 1 sets readlock
+        assert(rwflags==3); //check both read locks active
 #pragma omp atomic
-        read &= ~2;
+        rwflags &= ~2;
         write_lock w(lock);
 #pragma omp atomic
-        write |= 2;
+        rwflags |= 4;
         sleep(2);
-        write &= ~2;
+#pragma omp atomic
+        rwflags &= ~4;
       }
       running=false;
     }
