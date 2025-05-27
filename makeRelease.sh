fa71cf1f5c66cb9fbdb558781c643e84b194f78d
Russell Standish <hpcoder@hpcoders.com.au>
2025-05-27 15:45:25 +1000

    Add the Doxyversion string to the project name.

--- makeRelease.sh
+++ makeRelease.sh
@@ -2,7 +2,7 @@ echo "old version=`git describe`"
 echo '#define VERSION "'$1'"' >include/version.h
 echo 'VERSION='$1 >Makefile.version
 echo "Version $1" >doc/version.tex
-echo "PROJECT_NUMBER=$1" >Doxyversion
+echo "PROJECT_NAME=EcoLab:&nbsp;$1" >Doxyversion
 git commit -a -m "Release $1"
 git tag -a -m "" $1
 
