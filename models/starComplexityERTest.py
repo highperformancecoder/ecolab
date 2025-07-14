63459bb516ad0cfd7507e45ba3c5688a1738862f
Russell Standish <hpcoder@hpcoders.com.au>
2025-07-14 16:05:07 +1000

    Missing file for the experiment.

---
+++ models/starComplexityERTest.py
@@ -0,0 +1,11 @@
+from starComplexity import starC
+
+nodes=1000
+
+out=open(f'ER-{nodes}.csv', 'w')
+print("id,C, bar{*}", file=out)
+for i in range(1000):
+    links=int(nodes*(nodes-1)//2*starC.uni.rand())
+    g=starC.randomERGraph(nodes, links)
+    print(links,g.complexity(), g.starComplexity(),)
+    print(i,links,g.complexity(), g.starComplexity(),sep=', ',file=out)
