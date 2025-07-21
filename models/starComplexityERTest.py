b1b13c9cf6a21450cdcc33853a535b7b310a46fc
Russell Standish <hpcoder@hpcoders.com.au>
2025-07-21 12:37:22 +1000

    Fix header output.

--- models/starComplexityERTest.py
+++ models/starComplexityERTest.py
@@ -3,7 +3,7 @@ from starComplexity import starC
 nodes=1000
 
 out=open(f'ER-{nodes}.csv', 'w')
-print("id,C, bar{*}", file=out)
+print("id,links,C, bar{*}", file=out)
 for i in range(1000):
     links=int(nodes*(nodes-1)//2*starC.uni.rand())
     g=starC.randomERGraph(nodes, links)
