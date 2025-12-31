285542b4528537750e81528df0cbcd516ee244e4
Russell Standish <hpcoder@hpcoders.com.au>
2026-01-01 08:55:37 +1100

    Update dbm test

--- test/00/t0017a.sh
+++ test/00/t0017a.sh
@@ -7,34 +7,7 @@
 
 
 here=`pwd`
-if test $? -ne 0; then exit 2; fi
-tmp=/tmp/$$
-mkdir $tmp
-if test $? -ne 0; then exit 2; fi
-cd $tmp
-if test $? -ne 0; then exit 2; fi
-
-fail()
-{
-    echo "FAILED" 1>&2
-    cd $here
-    chmod -R u+w $tmp
-    rm -rf $tmp
-    exit 1
-}
-
-pass()
-{
-    echo "PASSED" 1>&2
-    cd $here
-    chmod -R u+w $tmp
-    rm -rf $tmp
-    exit 0
-}
-
-trap "fail" 1 2 3 15
-
-export LD_LIBRARY_PATH=/usr/local/lib:$HOME/usr/lib:$LD_LIBRARY_PATH
+. $here/test/common-test.sh
 
 $here/test/testNDBM
 if test $? -ne 0; then fail; fi
