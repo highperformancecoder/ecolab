32bde735e05e71bb2bd0f50b614022f60310bf84
Russell Standish <hpcoder@hpcoders.com.au>
2026-01-01 08:51:50 +1100

    Fix up condense test

--- test/00/t0016a.sh
+++ test/00/t0016a.sh
@@ -7,45 +7,20 @@
 
 
 here=`pwd`
-if test $? -ne 0; then exit 2; fi
-tmp=/tmp/$$
-mkdir $tmp
-if test $? -ne 0; then exit 2; fi
-cd $tmp
-if test $? -ne 0; then exit 2; fi
-
-fail()
-{
-    echo "FAILED" 1>&2
-    cd $here
-    chmod -R u+w $tmp
-    rm -rf $tmp
-    exit 1
-}
-
-pass()
-{
-    echo "PASSED" 1>&2
-    cd $here
-    chmod -R u+w $tmp
-    rm -rf $tmp
-    exit 0
-}
-
-trap "fail" 1 2 3 15
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+. $here/test/common-test.sh
 
 # Test condense functionality
-cat >input.tcl <<EOF
-ecolab.density {1 0 1 0 1}
-ecolab.make_consistent
-ecolab.repro_rate {1 1 1 1 1}
-ecolab.interaction.diag {1 1 1 1 1}
-ecolab.condense
-if {[ecolab.density]!={1 1 1}} {exit 1}
+cat >input.py <<EOF
+from ecolab_model import panmictic_ecolab as ecolab
+ecolab.density([1, 0, 1, 0, 1])
+ecolab.makeConsistent()
+ecolab.repro_rate([1, 1, 1, 1, 1])
+ecolab.interaction.diag([1, 1, 1, 1, 1])
+ecolab.condense()
+if ecolab.density()!=[1, 1, 1]: exit(1)
 EOF
 
-$here/models/ecolab input.tcl
+$here/bin/ecolab input.py
 if test $? -ne 0; then fail; fi
 
 pass
