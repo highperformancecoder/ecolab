a782491b9619089af530fbb12088b039f63d0359
Russell Standish <hpcoder@hpcoders.com.au>
2026-01-01 14:42:42 +1100

    Fix omp_rw_lock test

--- test/00/t0024a.sh
+++ test/00/t0024a.sh
@@ -6,22 +6,10 @@
 #  Open source licensed under the MIT license. See LICENSE for details.
 
 
+here=`pwd`
+. $here/test/common-test.sh
 
-fail()
-{
-    echo "FAILED" 1>&2
-    exit 1
-}
-
-pass()
-{
-    echo "PASSED" 1>&2
-    exit 0
-}
-
-trap "fail" 1 2 3 15
-
-test/test_omp_rw_lock
+$here/test/test_omp_rw_lock
 if test $? -ne 0; then fail; fi
 
 pass
